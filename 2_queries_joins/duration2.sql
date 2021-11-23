2a61b0191119e265a105a198e04c13689e2c82a4
Mahir Ahmed <mahir.ahmed56@hotmail.com>
2021-11-22 19:54:58 -0500

    3_queries

--- 2_queries_joins/duration2.sql
+++ 2_queries_joins/duration2.sql
@@ -3,3 +3,7 @@ FROM assignment_submissions
 JOIN cohorts ON cohorts.id = cohort_id
 JOIN students ON students.id = student_id
 WHERE cohorts.name = 'FEB12';
+
+
+
+
---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,2 @@
+SELECT day and total_submissions,
+FROM assignment_submissions
