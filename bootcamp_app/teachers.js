d817e3337218638cdfa24ff75f43da06e62a0074
Mahir Ahmed <mahir.ahmed56@hotmail.com>
2021-11-24 20:36:22 -0500

    Inject SQL protect

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,6 +6,10 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 
 pool.query(`
 SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
@@ -13,12 +17,12 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name =  '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teacher;
 `)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-}).catch(err => console.error('query error', err.stack));
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+  }).catch(err => console.error('query error', err.stack));
